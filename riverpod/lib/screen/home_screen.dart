0a9baf03c6def50acc3c48bad150bc22610b1bea
sungseokmin <jkl154527@gmail.com>
2024-03-08 08:41:21 +0900

    riverpod

---
+++ riverpod/lib/screen/home_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_tutorial/layout/default_layout.dart';
+import 'package:flutter_state_tutorial/screen/auto_dispose_modifier_screen.dart';
+import 'package:flutter_state_tutorial/screen/code_generation_screen.dart';
+import 'package:flutter_state_tutorial/screen/family_modifier_screen.dart';
+import 'package:flutter_state_tutorial/screen/future_provider_screen.dart';
+import 'package:flutter_state_tutorial/screen/listen_provider_screen.dart';
+import 'package:flutter_state_tutorial/screen/provider_screen.dart';
+import 'package:flutter_state_tutorial/screen/state_notifier_provier_screen.dart';
+import 'package:flutter_state_tutorial/screen/state_provider_screen.dart';
+import 'package:flutter_state_tutorial/screen/stream_provider_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'HomeScreen',
+      body: ListView(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const StateProviderScreen(),
+              ));
+            },
+            child: const Text(
+              'StateProviderScreen',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const StateNotifierProviderScreen(),
+              ));
+            },
+            child: const Text(
+              'StateNotifierProviderScreen',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const FutureProviderScreen(),
+              ));
+            },
+            child: const Text(
+              'FutureProviderScreen',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const StreamProviderScreen(),
+              ));
+            },
+            child: const Text(
+              'StreamProviderScreen',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const FamilyModifierScreen(),
+              ));
+            },
+            child: const Text(
+              'FamilyModifierScreen',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const AutoDisposeModifierScreen(),
+              ));
+            },
+            child: const Text(
+              'AutoDisposeModifierScreen',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const ListenProviderScreen(),
+              ));
+            },
+            child: const Text(
+              'ListenProviderScreen',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const ProviderScreen(),
+              ));
+            },
+            child: const Text(
+              'ProviderScreen',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const CodeGenerationScreen(),
+              ));
+            },
+            child: const Text(
+              'CodeGenerationScreen',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ riverpod/lib/screen/state_provider_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_state_tutorial/layout/default_layout.dart';
+import 'package:flutter_state_tutorial/riverpod/state_provider.dart';
+
+class StateProviderScreen extends ConsumerWidget {
+  const StateProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
+    return DefaultLayout(
+      title: 'StateProviderScreen',
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: const Text('UP'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).state =
+                    ref.read(numberProvider.notifier).state - 1;
+              },
+              child: const Text('DOWN'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
