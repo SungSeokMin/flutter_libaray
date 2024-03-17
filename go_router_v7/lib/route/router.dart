d24c26dd6e0c040d2ba938d9f4e4df4a742c1c43
sungseokmin <jkl154527@gmail.com>
2024-03-17 12:53:11 +0900

    context.go(path) 함수

--- go_router_v7/lib/route/router.dart
+++ go_router_v7/lib/route/router.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:go_router_v7/screens/basic_screen.dart';
+import 'package:go_router_v7/screens/1_basic_screen.dart';
 import 'package:go_router_v7/screens/root_screen.dart';
 
 final router = GoRouter(
