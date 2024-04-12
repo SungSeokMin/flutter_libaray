e8c4931e859440fa11ae8f61790c8104dad84866
sungseokmin <jkl154527@gmail.com>
2024-04-12 09:54:12 +0900

    Flutter RiverPod

--- lib_riverpod/lib/main.dart
+++ lib_riverpod/lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_state_tutorial/riverpod/provider_observer.dart';
 import 'package:flutter_state_tutorial/screen/home_screen.dart';
 
 void main() {
-  return runApp(
-    ProviderScope(
-      observers: [
-        Logger(),
-      ],
-      child: const MaterialApp(
-        home: HomeScreen(),
-      ),
-    ),
-  );
+  return runApp(const MaterialApp(
+    home: HomeScreen(),
+  ));
 }
