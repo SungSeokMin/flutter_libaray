25d2e3452991ac4f2d3e5b40aad6f94ac750eb5b
sungseokmin <jkl154527@gmail.com>
2024-04-15 18:00:56 +0900

    Path Parameters

--- lib_go_router/lib/route/router.dart
+++ lib_go_router/lib/route/router.dart
@@ -4,6 +4,7 @@ import 'package:go_router_v7/screens/2_named_screen.dart';
 import 'package:go_router_v7/screens/3_push_screen.dart';
 import 'package:go_router_v7/screens/4_pop_base_screen.dart';
 import 'package:go_router_v7/screens/5_pop_return_screen.dart';
+import 'package:go_router_v7/screens/6_path_param_screen.dart';
 import 'package:go_router_v7/screens/root_screen.dart';
 
 final router = GoRouter(
@@ -33,6 +34,16 @@ final router = GoRouter(
             GoRoute(
               path: 'return',
               builder: (context, state) => PopReturnScreen(),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: 'path_param/:id',
+          builder: (context, state) => PathParamScreen(),
+          routes: [
+            GoRoute(
+              path: ':name',
+              builder: (context, state) => PathParamScreen(),
             ),
           ],
         ),
